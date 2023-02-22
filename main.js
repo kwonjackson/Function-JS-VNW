96a3afa979f48c65b6581032ca8db6f3c5849c57
kwonjackson <kwonjackson@hotmail.com>
2023-02-22 18:59:56 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,46 @@
+//1 - crie uma função que exiba uma mensagem no console
+
+const função = 'Joy, toca k-pop';
+
+console.log(função);
+
+//2 - crie uma função que receba o seu nome como (parâmetro) e exiba no console
+
+const nome = 'Jackson';
+
+console.log(nome);
+
+//3 - crie uma função que receba nome, idade, e um estilo musical preferido (parâmetros) e exiba no console
+
+const infos = ['Jackson', 24, 'K-POP']
+
+console.log(infos)
+
+//4 - crie uma função que receba um filme, uma música (parâmetros) e exiba no console
+
+const favoritos = ['M3GAN','Shooting Star - XG'];
+
+console.log(favoritos)
+
+//5 - crie uma função que retorne o triplo do número recebido no parâmetro da função
+
+function triploDeTres() {
+    return 3 * 3 * 3;
+  }  
+
+  console.log(triploDeTres()); 
+
+//6 - crie uma função que  verifique se uma  variável é true ou false
+
+function beleza(soubonito) {
+    if (soubonito === true) {
+        return "tu é bonito";
+    } else if (naosoubonito === false) {
+        return "tu é feio";
+    }
+}
+
+const soubonito = true
+
+console.log(soubonito)
+
